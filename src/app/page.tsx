7f684410b379ba5c30077f4f89aea622b39d3d95
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 17:58:40 -0600

    add google sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import SignIn from "@/components/sign-in";
+import SignInGithub from "@/components/sign-in";
+import SignInGoogle from "@/components/SignInGoogle";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-3 mt-4">
       <h1 className="text-4xl font-bold">Welcome to Auth demo</h1>
-      <SignIn />
+      <p className="text-xl">Sign in with any of these options:</p>
+      <div className="flex justify-center gap-3">
+        <SignInGithub />
+        <SignInGoogle />
+      </div>
     </div>
   );
 }
---
+++ src/components/SignInGoogle.tsx
@@ -0,0 +1,18 @@
+
+import { signIn } from "@/auth"
+ 
+export default function SignInGoogle() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <button
+        className="px-4 py-2 text-white bg-indigo-900 rounded-lg duration-150 hover:bg-indigo-700 active:shadow-lg"
+        type="submit"
+      >Sign in with Google</button>
+    </form>
+  )
+} 
