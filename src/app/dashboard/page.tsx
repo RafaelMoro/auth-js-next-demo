a3d44fb691dc70466eeb81b6f1ecac125ecd5613
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 17:20:02 -0600

    add sign out into dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { SignOut } from "@/components/sign-out";
+
 export default function DashboardPage() {
   return (
     <main className="flex flex-col items-center gap-3 mt-4">
       <h1 className="text-4xl font-bold">Dashboard</h1>
+      <SignOut />
     </main>
   )
 }
