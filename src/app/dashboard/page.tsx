346f4ffda28f3823a1904ba461e0faa020e722a1
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 17:17:37 -0600

    add dashboard route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <main className="flex flex-col items-center gap-3 mt-4">
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+    </main>
+  )
+}
