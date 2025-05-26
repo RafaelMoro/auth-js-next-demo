091a7bfca2816639819f000f01fa7ceaadec9753
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 17:27:58 -0600

    show session info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import { redirect } from 'next/navigation'
 import { SignOut } from "@/components/sign-out";
+import UserAvatar from "@/components/UserAvatar";
+import { auth } from "../../auth"
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await auth()
+
+  if (!session?.user) {
+    redirect('/')
+  }
+
   return (
     <main className="flex flex-col items-center gap-3 mt-4">
-      <h1 className="text-4xl font-bold">Dashboard</h1>
+      <h1 className="text-4xl font-bold">Welcome {session?.user?.name}</h1>
+      <UserAvatar imgSrc={session.user.image ?? ""} />
       <SignOut />
     </main>
   )
